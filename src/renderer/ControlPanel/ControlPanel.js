449f3cc54df824a7576c6c9beb9b764e994ef10e
Andrew Morris <voltrevo@gmail.com>
2017-08-19 22:14:42 +1000

    Deactivate hover on animation

--- src/renderer/ControlPanel/ControlPanel.js
+++ src/renderer/ControlPanel/ControlPanel.js
@@ -6,9 +6,20 @@ const Element = require('./Element.html');
 
 const ControlPanel = () => {
   const el = Element();
+  const icon = el.querySelector('.icon');
 
   el.querySelector('.icon').addEventListener('click', () => {
     el.classList.toggle('expanded');
+    icon.classList.add('disable-hover');
+
+    let handler;
+    window.addEventListener(
+      'mousemove',
+      (handler = () => {
+        window.removeEventListener('mousemove', handler);
+        icon.classList.remove('disable-hover');
+      }),
+    );
   });
 
   return el;
